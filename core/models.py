11851b8b0fa46ae36c1bda13934cdeed53880881
Sanoth-debnath-cse <sanoth14@cse.pstu.ac.bd>
2025-03-09 19:56:09 +0600

    Custom user manager implemented

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 from shared.base_model import BaseModel
+from .managers import CustomUserManager
 
 
 # Create your models here.
@@ -15,7 +16,11 @@ class User(AbstractBaseUser,PermissionsMixin,BaseModel):
     is_active=models.BooleanField(default=True)
     is_varified=models.BooleanField(default=True)
     image=models.ImageField(blank=True,null=True)
+    is_superuser=models.BooleanField(default=False)
+    is_staff=models.BooleanField(default=False)
 
     USERNAME_FIELD ="phone"
     REQUIRED_FIELDS=[]
+
+    objects=CustomUserManager()
 
